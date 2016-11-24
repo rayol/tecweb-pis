67ad6da49d92717003a312614cbbb31e29a7c74c
cloudera <cloudera@quickstart.cloudera>
2016-11-24 11:09:43 -0800

    projeto 3 v.1

---
+++ projeto3/projeto3.md~
@@ -0,0 +1,73 @@
+# Questão 1. Qual é o tamanho do seu índice comprimido para a 1a. coleção?
+3376 bytes
+
+
+# Questão 2. Qual é o tamanho do seu índice comprimido para a 2a. coleção?
+
+105349 bytes
+
+
+# Questão 3. Que artigos da 2a. coleção satisfazem as seguintes consultas boleanas? apresente somente os títulos.
+
+**outrageous AND fortune**
+
+28185317	
+66505266
+68362764
+86199064
+387715105
+
+**means AND deceit**
+
+33020264
+35594465
+35998866
+127948088
+160785063
+181807837
+226906357
+356413073
+
+
+**(white OR red ) AND rose AND pluck**
+
+*Não houve nenhuma ocorrencia com a busca*
+
+**(unhappy OR outrageous OR (good AND your)) AND fortune**
+
+5143869
+23104749
+57672614
+65285579
+66583576
+68362764
+69493205
+72843446
+75657751	
+93268779
+102583980
+102728026
+125656060
+145201777
+180364742
+194179236
+226751446
+233586925
+259580153	
+260751892	
+276161013	
+284575884	
+297315471	
+306011618	
+308776721	
+311528040	
+311679190	
+345688268	
+346318884	
+355066293	
+385905007	
+387715105	
+393557584	
+412986991	
+416448761	
+
---
+++ projeto3/src/main/java/tl/lin/data/queue/TopScoredInts.java
@@ -0,0 +1,108 @@
+/*
+ * Lintools: tools by @lintool
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package tl.lin.data.queue;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.util.PriorityQueue;
+
+import tl.lin.data.pair.PairOfIntFloat;
+
+public class TopScoredInts implements Writable {
+  private class ScoredIntPriorityQueue extends PriorityQueue<PairOfIntFloat> {
+    private ScoredIntPriorityQueue(int maxSize) {
+      super.initialize(maxSize);
+    }
+
+    @Override
+    protected boolean lessThan(Object obj0, Object obj1) {
+      // If equal scores, break tie by int descending.
+      if (((PairOfIntFloat) obj0).getValue() == ((PairOfIntFloat) obj1).getValue()) {
+        return ((PairOfIntFloat) obj0).getKey() < ((PairOfIntFloat) obj1).getKey() ? true : false;
+      }
+
+      return (((PairOfIntFloat) obj0).getValue() < ((PairOfIntFloat) obj1).getValue()) ? true
+          : false;
+    }
+  }
+
+  private ScoredIntPriorityQueue queue;
+  private int maxElements;
+
+  // TODO: Add the option to control how to break scoring ties
+
+  public TopScoredInts() {
+    this(10);
+  }
+
+  public TopScoredInts(int n) {
+    queue = new ScoredIntPriorityQueue(n);
+    maxElements = n;
+  }
+
+  public void add(int n, float f) {
+    queue.insert(new PairOfIntFloat(n, f));
+  }
+
+  public int getMaxElements() {
+    return maxElements;
+  }
+
+  public int size() {
+    return queue.size();
+  }
+
+  public PairOfIntFloat[] extractAll() {
+    int len = queue.size();
+    PairOfIntFloat[] arr = (PairOfIntFloat[]) new PairOfIntFloat[len];
+    for (int i = 0; i < len; i++) {
+      arr[len - 1 - i] = queue.pop();
+    }
+    return arr;
+  }
+
+  @Override
+  public void readFields(DataInput in) throws IOException {
+    maxElements = in.readInt();
+    queue = new ScoredIntPriorityQueue(maxElements);
+
+    int n = in.readInt();
+    for (int i = 0; i < n; i++) {
+      int k = in.readInt();
+      float v = in.readFloat();
+      add(k, v);
+    }
+  }
+
+  @Override
+  public void write(DataOutput out) throws IOException {
+    out.writeInt(maxElements);
+    int sz = size();
+    out.writeInt(sz);
+
+    // Note: serialization is destructive!
+    PairOfIntFloat pair;
+    for (int i = 0; i < sz; i++) {
+      pair = queue.pop();
+      out.writeInt(pair.getKey());
+      out.writeFloat(pair.getValue());
+    }
+  }
+}
